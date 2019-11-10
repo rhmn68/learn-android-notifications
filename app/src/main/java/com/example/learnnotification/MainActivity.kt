5548ca29a9b7017d049b1b47ddfd7b706d74c10d
rhmn68 <auliar68@gmail.com>
2019-11-10 21:09:57 +0700

    add notif pending intent

--- app/src/main/java/com/example/learnnotification/MainActivity.kt
+++ app/src/main/java/com/example/learnnotification/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.learnnotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +26,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button.setOnClickListener {
+            sendNotification(it)
+        }
+    }
+
+    fun sendNotification(view: View){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.ic_notifications_black_48dp)
             .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_black_48dp))
-            .setContentTitle(resources.getString(R.string.content_text))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
             .setSubText(resources.getString(R.string.subtext))
             .setAutoCancel(true)
 
