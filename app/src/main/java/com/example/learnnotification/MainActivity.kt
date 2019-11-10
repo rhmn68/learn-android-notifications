02cc6001641a7bacba009179c1f0ea6fb393c615
rhmn68 <auliar68@gmail.com>
2019-11-10 20:56:05 +0700

    add simple notification

---
+++ app/src/main/java/com/example/learnnotification/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.learnnotification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val NOTIFICATION_ID = 1
+        var CHANNEL_ID = "channel_01"
+        var CHANNEL_NAME: CharSequence = "dicoding channel"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications_black_48dp)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications_black_48dp))
+            .setContentTitle(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        /*
+        *  Untuk android Oreo ke atas perlu menambahkan notification channel
+        * */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            mBuilder.setChannelId(CHANNEL_ID)
+
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+    }
+}
